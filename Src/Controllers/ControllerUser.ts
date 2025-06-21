3ead591ed4c7de6a76c7646f3e6c866cdc9324f0
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-06-21 12:59:25 -0300

    feat: controller user base created

---
+++ Src/Controllers/ControllerUser.ts
@@ -0,0 +1,19 @@
+import {Request, Response} from "express";
+
+export class ControllerUser {
+    async saveUser(request: Request, response: Response): Promise<Response> {
+        return response.json();
+    }
+
+    async getUserLogin(request: Request, response: Response): Promise<Response> {
+        return response.json();
+    }
+
+    async updateUser(request: Request, response: Response): Promise<Response> {
+        return response.json();
+    }
+
+    async deleteUser(request: Request, response: Response): Promise<Response> {
+        return response.json();
+    }
+}

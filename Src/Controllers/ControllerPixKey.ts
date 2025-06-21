019ffce6968896ff85d81ed019de2d80314abdbe
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-06-21 13:13:07 -0300

    feat: controller pix key base created

---
+++ Src/Controllers/ControllerPixKey.ts
@@ -0,0 +1,19 @@
+import {Request, Response} from "express";
+
+export class ControllerPixKey {
+    async savePixKey(request: Request, response: Response): Promise<Response> {
+        return response.json();
+    }
+
+    async getPixKey(request: Request, response: Response): Promise<Response> {
+        return response.json();
+    }
+
+    async updatePixKey(request: Request, response: Response): Promise<Response> {
+        return response.json();
+    }
+
+    async deletePixKey(request: Request, response: Response): Promise<Response> {
+        return response.json();
+    }
+}

31beb60a78d19e5d16b9f4e862957be27ef67f2c
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-06-23 11:43:07 -0300

    except: generic exceptions created

---
+++ Src/Exceptions/OperaException.ts
@@ -0,0 +1,7 @@
+import {BaseException} from "@Exceptions/BaseException";
+
+export class OperaException extends BaseException {
+    constructor(message: string, code: number = 400) {
+        super(message, code);
+    }
+}

bb9e7079889ecc216ca3b001fd7a26dfd1152408
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-06-23 15:39:11 -0300

    feat: invoice utility created

---
+++ Src/Utils/InvoiceUtil.ts
@@ -0,0 +1,36 @@
+import {InvoiceDTO} from "@Dtos/InvoiceDTO";
+import {InvoiceException} from "@Exceptions/InvoiceException";
+
+export class InvoiceUtil {
+    private static checkDescription(description: string): void {
+        const regDescription = (
+            /^[A-Z][a-zA-Z- .]{1,253}[a-zA-Z.]$/
+        );
+
+        if (description.match(regDescription) === null) throw new InvoiceException (
+            "invalid characters found in money box description"
+        );
+    }
+
+    private static checkBalance(balance: number, typeBalance: string = ""): void {
+        if (balance < 0) throw new InvoiceException(
+            `invoice${typeBalance} value must be positive`
+        );
+    }
+
+    private static checkQuantity(quantity: number): void {
+        if (quantity < 0) throw new InvoiceException(
+            "quantity value must be positive"
+        );
+    }
+
+    public static checkInvoice(invoice: InvoiceDTO): void {
+        const description: any = invoice.description;
+
+        if (description) this.checkDescription(invoice.description as string);
+
+        this.checkQuantity(invoice.quantity);
+        this.checkBalance(invoice.invoicePaid, " paid balance");
+        this.checkBalance(invoice.balanceValue, " balance");
+    }
+}

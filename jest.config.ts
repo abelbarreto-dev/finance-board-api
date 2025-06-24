eebbf6f5be7e1e66dcd2c28920b37df488d10624
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-06-24 16:09:58 -0300

    test: ts-jest configured

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  moduleNameMapper: {
+    "^@Utils/(.*)$": "<rootDir>/Src/Utils/$1",
+    "^@Exceptions/(.*)$": "<rootDir>/Src/Exceptions/$1",
+  },
+};

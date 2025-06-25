2582084bcd0a945a83022746cb435e9a57f4cd68
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-06-25 11:50:36 -0300

    feat: card description and card type added

--- Src/Dtos/CardDTO.ts
+++ Src/Dtos/CardDTO.ts
@@ -1,7 +1,10 @@
 import {EnumFlag} from "@Enums/EnumFlag";
+import {EnumCard} from "@Enums/EnumCard";
 
 export interface CardDTO {
     bankId: number;
+    description: string;
+    cardType: EnumCard;
     cardFlag: EnumFlag;
     cardLimit: number;
     currentLimit: number;
--- Src/Models/Card.ts
+++ Src/Models/Card.ts
@@ -1,8 +1,11 @@
 import {EnumFlag} from "@Enums/EnumFlag";
+import {EnumCard} from "@Enums/EnumCard";
 
 export class Card {
     id: number = 0;
     bankId: number = 0;
+    description: string = "";
+    cardType: EnumCard = EnumCard.PHYSICAL;
     cardFlag: EnumFlag = EnumFlag.MASTERCARD;
     cardLimit: number = 0;
     currentLimit: number = 0;

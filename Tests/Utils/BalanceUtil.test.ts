a7a8f096745da54f00a591b39a2e348a88960986
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-06-27 13:05:01 -0300

    fix: balance util tests improved

--- Tests/Utils/BalanceUtil.test.ts
+++ Tests/Utils/BalanceUtil.test.ts
@@ -1,99 +1,40 @@
 import {BalanceUtil} from "@Utils/BalanceUtil";
-import {BankException} from "@Exceptions/BankException";
-import {CardException} from "@Exceptions/CardException";
-import {InvoiceException} from "@Exceptions/InvoiceException";
-import {MoneyException} from "@Exceptions/MoneyException";
-import {OperaException} from "@Exceptions/OperaException";
 import {BaseException} from "@Exceptions/BaseException";
 
 describe("BalanceUtil", () => {
-    const excepts = {
-        base: new BaseException(""),
-        bank: new BankException(""),
-        card: new CardException(""),
-        invoice: new InvoiceException(""),
-        money: new MoneyException(""),
-        opera: new OperaException("")
-    }
-
-    let message: string = "";
-
     test("Positive Balance", () => {
-        const testSuccess = expect(() => {
-            BalanceUtil.checkBalance<BaseException>(25, excepts.base);
-        });
-
-        testSuccess.not.toThrow(excepts.base);
-    });
-
-    test("BaseException Handled", () => {
-        message = "negative balance error";
-        excepts.base.message = message;
+        const message = "try error found in balance";
+        const except = new BaseException(message);
 
         const testSuccess = expect(() => {
-            BalanceUtil.checkBalance<BaseException>(-0.00000001, excepts.base);
+            BalanceUtil.checkBalance<BaseException>(25, except);
         });
 
-        testSuccess.toThrow(excepts.base);
+        testSuccess.not.toThrow(except);
+        testSuccess.not.toThrow(message);
     });
 
-    test("BankException Handled", () => {
-        message = "invalid bank balance value found, bank balance value must be positive";
-        excepts.bank.message = message;
+    test("Balance Util Success - Balance Zero", () => {
+        const message = "try error found in balance equals to zero";
+        const except = new BaseException(message);
 
-        const testExcept = expect(() => {
-            BalanceUtil.checkBalance<BankException>(-25, excepts.bank);
+        const testSuccess = expect(() => {
+            BalanceUtil.checkBalance<BaseException>(0, except);
         });
 
-        testExcept.toThrow(message);
-        testExcept.toThrow(excepts.bank);
+        testSuccess.not.toThrow(except);
+        testSuccess.not.toThrow(message);
     });
 
-    test("CardException Handled", () => {
-        message = "invalid card balance value found, card balance value must be positive";
-        excepts.card.message = message;
+    test("Balance Util Error - Balance Negative - BaseException Handled", () => {
+        const message = "negative balance error";
+        const except = new BaseException(message);
 
-        const testExcept = expect(() => {
-            BalanceUtil.checkBalance<CardException>(-0.5, excepts.card);
+        const testSuccess = expect(() => {
+            BalanceUtil.checkBalance<BaseException>(-0.00000001, except);
         });
 
-        testExcept.toThrow(message);
-        testExcept.toThrow(excepts.card);
-    });
-
-    test("InvoiceException Handled", () => {
-        message = "invalid invoice balance value found, invoice balance value must be positive";
-        excepts.invoice.message = message;
-
-        const testExcept = expect(() => {
-            BalanceUtil.checkBalance<InvoiceException>(-24, excepts.invoice);
-        });
-
-        testExcept.toThrow(message);
-        testExcept.toThrow(excepts.invoice);
-    });
-
-    test("MoneyException Handled", () => {
-        message = "invalid money box balance value found, money box balance value must be positive";
-        excepts.money.message = message;
-
-        const testExcept = expect(() => {
-            BalanceUtil.checkBalance<MoneyException>(-0.01, excepts.money);
-        });
-
-        testExcept.toThrow(message);
-        testExcept.toThrow(excepts.money);
-    });
-
-    test("OperaException Handled", () => {
-        message = "invalid operation balance value found, operation balance value must be positive";
-        excepts.opera.message = message;
-
-        const testExcept = expect(() => {
-            BalanceUtil.checkBalance<OperaException>(-2.589, excepts.opera);
-        });
-
-        testExcept.toThrow(message);
-        testExcept.toThrow(excepts.opera);
+        testSuccess.toThrow(except);
+        testSuccess.toThrow(message);
     });
 });

324eb723f22a832203e184f75ef496a82bd54b97
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-06-30 12:15:57 -0300

    feat: bcrypt and service user being built

--- Src/Services/ServiceUser.ts
+++ Src/Services/ServiceUser.ts
@@ -1,15 +1,39 @@
 import {UserDTO} from "@Dtos/UserDTO";
 import {User} from "../Models/User";
 import {UserUtil} from "@Utils/UserUtil";
+import {genSalt, hash} from "bcrypt";
+import * as process from "node:process";
 
 export class ServiceUser {
+    private readonly _salt = process.env.SALT_NUMBER || "0";
+
+    private get salt(): number {
+        return parseInt(this._salt);
+    }
+
     async saveUser(userDTO: UserDTO): Promise<User> {
         UserUtil.checkUser(userDTO);
+
+        const salt: string = await genSalt(this.salt);
+
+        const passwdCrypted = await hash(userDTO.password, salt);
+
+        userDTO = {
+            ...userDTO,
+            password: passwdCrypted
+        } as UserDTO;
 
         return new User();
     }
 
     async getUserLogin(userDTO: UserDTO): Promise<User> {
+        const passwdCrypted = await hash(userDTO.password, this.salt);
+
+        userDTO = {
+            ...userDTO,
+            password: passwdCrypted
+        } as UserDTO;
+
         return new User();
     }
 

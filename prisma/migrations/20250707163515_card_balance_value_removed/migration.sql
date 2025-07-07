fab7172b14036ab0ce5942851fe063f769b66475
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-07-07 13:35:59 -0300

    message: remove card.balanceValue from card table

---
+++ prisma/migrations/20250707163515_card_balance_value_removed/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `balance_value` on the `Card` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Card" DROP COLUMN "balance_value";

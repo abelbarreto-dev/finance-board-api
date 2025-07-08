c1fd677a5c1b7aa1caafb69cc916f78242e8104a
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-07-08 12:51:27 -0300

    fix: route prefix at user routes

--- Src/Routes/UserRoutes.ts
+++ Src/Routes/UserRoutes.ts
@@ -5,27 +5,27 @@ import {userAuthentication} from "@Middlewares/UserAuthMiddleware";
 const userRoutes: Router = Router();
 const controller: ControllerUser = new ControllerUser();
 
-userRoutes.post("/user", async (request: Request, response: Response) => {
+userRoutes.post("", async (request: Request, response: Response) => {
     await controller.saveUser(request, response);
 });
 
-userRoutes.post("/user/login", async (request: Request, response: Response) => {
+userRoutes.post("/login", async (request: Request, response: Response) => {
     await controller.getUserLogin(request, response);
 });
 
-userRoutes.post("/user/recovery", async (request: Request, response: Response) => {
+userRoutes.post("/recovery", async (request: Request, response: Response) => {
     await controller.reactiveUser(request, response);
 });
 
-userRoutes.patch("/user/update/{id}", userAuthentication, async (request: Request, response: Response) => {
+userRoutes.patch("/update/{id}", userAuthentication, async (request: Request, response: Response) => {
     await controller.updateUser(request, response);
 });
 
-userRoutes.post("/user/deactivate", userAuthentication, async (request: Request, response: Response) => {
+userRoutes.post("/deactivate", userAuthentication, async (request: Request, response: Response) => {
     await controller.deactivateUser(request, response);
 });
 
-userRoutes.post("/user/logout", userAuthentication, async (request: Request, response: Response) => {
+userRoutes.post("/logout", userAuthentication, async (request: Request, response: Response) => {
     await controller.logout(request, response);
 });
 

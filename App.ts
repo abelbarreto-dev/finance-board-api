cf11e7c98ca4c93d98e64226778609512c24dae1
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-07-09 14:55:50 -0300

    feat: bank routes created

--- App.ts
+++ App.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import userRoutes from "@Routes/UserRoutes";
+import bankRoutes from "@Routes/BankRoutes";
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 8080;
 
 app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/bank", bankRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
---
+++ Src/Routes/BankRoutes.ts
@@ -0,0 +1,28 @@
+import {Router, Request, Response} from "express";
+import {ControllerBank} from "@Controllers/ControllerBank";
+import {userAuthentication} from "@Middlewares/UserAuthMiddleware";
+
+const bankRoutes: Router = Router();
+const controller: ControllerBank = new ControllerBank();
+
+bankRoutes.post("", userAuthentication, async (request: Request, response: Response) => {
+    await controller.saveBank(request, response);
+});
+
+bankRoutes.get("/:id", userAuthentication, async (request: Request, response: Response) => {
+    await controller.getBankById(request, response);
+});
+
+bankRoutes.get("/:id/all", userAuthentication, async (request: Request, response: Response) => {
+    await controller.getBanks(request, response);
+});
+
+bankRoutes.patch("/:id", userAuthentication, async (request: Request, response: Response) => {
+    await controller.updateBank(request, response);
+});
+
+bankRoutes.delete("/:id", userAuthentication, async (request: Request, response: Response) => {
+    await controller.deleteBank(request, response);
+});
+
+export default bankRoutes;
